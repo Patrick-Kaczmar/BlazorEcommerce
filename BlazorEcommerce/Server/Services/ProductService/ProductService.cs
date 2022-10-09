7f6494f7ef7d9eb09c79d1ad99318953959659ff
Patrick Kaczmar <pk1blue@yahoo.com>
2022-10-09 13:02:17 -0400

    Include Product variants & Types in product services

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -1,4 +1,6 @@
-﻿namespace BlazorEcommerce.Server.Services.ProductService
+﻿using BlazorEcommerce.Shared;
+
+namespace BlazorEcommerce.Server.Services.ProductService
 {
     public class ProductService : IProductService
     {
@@ -12,7 +14,10 @@
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
             var response = new ServiceResponse<Product>();
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                .Include(p => p.variants)
+                .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == productId);
             if(product == null)
             {
                 response.Success = false;
@@ -30,7 +35,7 @@
         {
             var response = new ServiceResponse<List<Product>>()
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products.Include(p => p.variants).ToListAsync()
             };
 
             return response;
@@ -40,7 +45,10 @@
         {
             var response = new ServiceResponse<List<Product>>
             {
-                Data = await _context.Products.Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower())).ToListAsync()
+                Data = await _context.Products
+                .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                .Include(p => p.variants)
+                .ToListAsync()
             };
 
             return response;
