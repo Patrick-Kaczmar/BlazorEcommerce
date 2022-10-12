d46b37653cbdcaf11877be0927a96c5933d94d25
Patrick Kaczmar <pk1blue@yahoo.com>
2022-10-11 20:25:55 -0400

    Update quantity with input number field

--- BlazorEcommerce/Shared/CartProductResponse.cs
+++ BlazorEcommerce/Shared/CartProductResponse.cs
@@ -13,6 +13,7 @@ namespace BlazorEcommerce.Shared
         public int ProductTypeId { get; set; }
         public string ProductType { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
-        public decimal Price { get; set; } 
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
