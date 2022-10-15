abafbdec089fd81263682e4d975b99ac44043819
Patrick Kaczmar <pk1blue@yahoo.com>
2022-10-15 18:06:22 -0400

    Added a logout option with authorize view component

--- BlazorEcommerce/Client/CustomAuthStateProvider.cs
+++ BlazorEcommerce/Client/CustomAuthStateProvider.cs
@@ -16,6 +16,7 @@ namespace BlazorEcommerce.Client
             _localStorageService = localStorageService;
             _http = http;
         }
+
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
             string authToken = await _localStorageService.GetItemAsStringAsync("authToken");
@@ -28,7 +29,8 @@ namespace BlazorEcommerce.Client
                 try
                 {
                     identity = new ClaimsIdentity(ParseClaimsFromJwt(authToken), "jwt");
-                    _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken.Replace("\"", ""));
+                    _http.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue("Bearer", authToken.Replace("\"", ""));
                 }
                 catch
                 {
@@ -47,19 +49,20 @@ namespace BlazorEcommerce.Client
 
         private byte[] ParseBase64WithoutPadding(string base64)
         {
-            switch(base64.Length % 4)
+            switch (base64.Length % 4)
             {
                 case 2: base64 += "=="; break;
-                case 3: base64 += "=="; break;
+                case 3: base64 += "="; break;
             }
             return Convert.FromBase64String(base64);
         }
-         
+
         private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
         {
             var payload = jwt.Split('.')[1];
             var jsonBytes = ParseBase64WithoutPadding(payload);
-            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            var keyValuePairs = JsonSerializer
+                .Deserialize<Dictionary<string, object>>(jsonBytes);
 
             var claims = keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
 
